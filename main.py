e17ce7f493efb0f683a2ef3d72afe1d395a55217
Michael Peralta <peralta.michael27@gmail.com>
2023-12-31 13:57:58 -0500

    Add collision with food logic to move the food to a random location when collision with snake head.

--- main.py
+++ main.py
@@ -27,7 +27,10 @@ count = 0
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
